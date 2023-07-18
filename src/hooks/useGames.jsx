a5b621580783acbc58dc23b8edf16ec5459554cf
Mohammed Usama <usamabhakrani@gmail.com>
2023-07-18 13:32:20 +0500

    Loading Skeletons

--- src/hooks/useGames.jsx
+++ src/hooks/useGames.jsx
@@ -4,15 +4,23 @@ import api from "../services/api";
 const useGames = () => {
   const [games, setGames] = useState([]);
   const [error, setError] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   useEffect(() => {
+    setIsLoading(true);
     api
       .get("games")
-      .then((res) => setGames(res.data.results))
-      .catch((err) => setError(err.message));
+      .then((res) => {
+        setGames(res.data.results);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, []);
 
-  return { games, error };
+  return { games, error, isLoading };
 };
 
 export default useGames;
